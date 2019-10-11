fbbef7add35d851e9080354dd6e88cf3eea122de
sinkosi <sinkosi@student.wethinkcode.co.za>
2019-10-11 16:58:24 +0200

    Well done Sibo

--- Day01/ex07/rostring.php
+++ Day01/ex07/rostring.php
@@ -4,12 +4,8 @@ if (isset($argv[1]))
 {
     $str = array_filter(explode(' ', $argv[1]));
     $rostring = array_shift($str);
-    $i = 0;
-    while ($i < sizeof($str))
-    {
-        print($str[$i]." ");
-        $i++;
-    }
-    print ($rostring);
+    $str[] = $rostring;
+    $str = implode($str, " ");
+    print ($str);
 }
 ?>
--- Day01/ex09/ssap2.php
+++ Day01/ex09/ssap2.php
@@ -1,5 +1,18 @@
 #!/usr/bin/php
-<?php 
+<?php
+function ft_isdigit($c){
+    if ($c >= '0' && $c <= '9')
+        return (1);
+    else
+        return (0);
+}
+
+function ft_isalpha($c){
+    if(($c >= 'a' && $c <= 'z') || ($c >= 'A' && $c <= 'Z'))
+        return (1);
+    else
+        return (0);
+}
     unset($argv[0]);//remove argv[0] from argv array
     $str = array();
     foreach ($argv as $value) {
@@ -12,7 +25,22 @@
         else
             array_push($str, $value);
     }
-    asort($str);
+    usort($str, strcasecmp);
+    //$arr_num = array();
     foreach($str as $value)
+    {
+        if (ft_isdigit($value[0])== 1)
+            $arr_num[] = $value;
+        //print ($value."\tIm a number\n");
+        else if ((ft_isalpha($value[0]) == 0) && (ft_isdigit($value[0]) == 0))
+            $arr_char[] = $value;    
+        //print ($value."\tI'm not alphanumeric\n");
+        else
+            $arr_str[] = $value;
+        //    print ($value."\tIm sorted\n");
+    }
+    $super = array_merge($arr_str, $arr_num);
+    $super = array_merge($super, $arr_char);
+    foreach($super as $value)
         print ($value."\n");
 ?>
